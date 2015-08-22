f8033a2f2066787bf59c3e7e4b25e555b76b9164
Diego Eis <diegoeis@gmail.com>
2015-08-22 14:33:30 -0300

    menu

---
+++ content/themes/diegoeis/assets/js/scripts.js
@@ -0,0 +1,23 @@
+var deTheme = (function(){
+  'use strict'
+
+  function init() {
+    sandwichMenu();
+  }
+
+  function sandwichMenu() {
+    var sandwich = document.getElementsByClassName('de-sandwich')[0];
+    var menu = document.getElementsByClassName('de-menu')[0];
+    sandwich.addEventListener('click', function(){
+      menu.classList.toggle('de-sidebar-visible');
+      sandwich.classList.toggle('de-sidebar-visible');
+    });
+  }
+
+  return {
+    init: init
+  }
+
+}());
+
+window.onload = deTheme.init();
