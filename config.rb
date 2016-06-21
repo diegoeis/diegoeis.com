7f47d6ca5cb8b2c8edd0a42c396e47dd7193f650
Diego Eis <diegoeis@gmail.com>
2016-06-20 21:50:48 -0300

    config

--- config.rb
+++ config.rb
@@ -102,13 +102,3 @@ configure :build do
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
 end
-activate :deploy do |deploy|
-  deploy.deploy_method = :rsync
-  deploy.host          = 'diegoeis.com'
-  deploy.path          = '/var/www/diegoeis.com'
-  # Optional Settings
-  # deploy.user  = 'tvaughan' # no default
-  # deploy.port  = 5309 # ssh port, default: 22
-  # deploy.clean = true # remove orphaned files on remote host, default: false
-  # deploy.flags = '-rltgoDvzO --no-p --del' # add custom flags, default: -avz
-end
